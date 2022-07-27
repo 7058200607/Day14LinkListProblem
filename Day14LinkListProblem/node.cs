96220f2677a76acd482a60f9a9baa78fade9d1f6
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-27 23:16:23 +0530

    insert Element in Linklist 56-30-70

---
+++ Day14LinkListProblem/node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14LinkListProblem
+{
+    public class node
+    {
+        public int data;
+        public node next;
+        public node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
